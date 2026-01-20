e14e2d5efa1801763fef49734d745ad4610edfbe
BjornOdinsson89 <bjornodinsson89@gmail.com>
2026-01-20 10:56:26 -0700

    Bump version to 1.0.0 and update URLs

--- OdinTradeLedger.user.js
+++ OdinTradeLedger.user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name         Odin Trade Ledger (Weav3r Receipt + Pricelist)
-// @version      0.9.41
+// @version      1.0.0
+// @downloadURL  https://github.com/bjornodinsson89/Odin-Trade-Ledger/raw/main/OdinTradeLedger.user.js
+// @updateURL    https://github.com/bjornodinsson89/Odin-Trade-Ledger/raw/main/OdinTradeLedger.meta.js
 // @author       BjornOdinsson89
 // @description  Trade drawer: reads trade log, shows market/lowest bazaar/buy/profit, caches your Weav3r pricelist + receipt
 // @icon         https://i.postimg.cc/BQ6bSYKM/file-000000004bb071f5a96fc52564bf26ad-(1).png
